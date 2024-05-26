83e209738c31119d2f6784822d1547ba6100a537
Adil Fathi <54827419+adilfathi@users.noreply.github.com>
2024-05-26 20:27:51 +0700

    Sum Jumlah Laporan Done

--- app/Http/Controllers/warga/TrenPelaporanController.php
+++ app/Http/Controllers/warga/TrenPelaporanController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\warga;
 
 use App\Http\Controllers\Controller;
+use App\Models\LaporanModel;
 use Illuminate\Http\Request;
 
 class TrenPelaporanController extends Controller
 {
-    //
+    function sumLaporan()
+    {
+        $total_Laporan = LaporanModel::count();
+        return response()->json([
+            'judul' => 'Jumlah Laporan',
+            'total_items' => $total_Laporan
+        ]);
+    }
 }
